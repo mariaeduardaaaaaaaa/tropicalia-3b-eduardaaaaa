8a7587507c520cde4732edbb0e216941708fcbb3
mariaeduardaaaaaaaa <146455966+mariaeduardaaaaaaaa@users.noreply.github.com>
2025-04-01 08:23:14 -0300

    01/04/2025

--- scrip.js
+++ scrip.js
@@ -1,4 +1,10 @@
 document.addEventListener(DOMcontentLoaded ,function(){
     const aumentarFonteBotao=document.getElementByld(aumentarFonte);
-    let tamanhoatualFonte
+
+    let tamanhoatualFonte=1;
+
+aumentarFonteBotao.addEventListener('clik,function'){
+    tamanhoatualFonte+=0.1;
+    document.body.style.fontSize =${tamanhoatualFonte}rem ;
+});
 })
