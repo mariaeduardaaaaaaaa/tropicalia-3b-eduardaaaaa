2e9b4513eb8284ae685d17f300c5bbecd56520c9
mariaeduardaaaaaaaa <146455966+mariaeduardaaaaaaaa@users.noreply.github.com>
2025-04-01 18:48:13 -0300

    01/04/2025

--- scrip.js
+++ scrip.js
@@ -1,10 +1,16 @@
-document.addEventListener(DOMcontentLoaded ,function(){
-    const aumentarFonteBotao=document.getElementByld(aumentarFonte);
-
-    let tamanhoatualFonte=1;
-
-aumentarFonteBotao.addEventListener('clik,function'(){
-    tamanhoatualFonte+=0.1;
-    document.body.style.fontSize =`${tamanhoAtualFonte}rem`;
-});
-});
+document.addEventListener('DOMContentLoaded', function(){
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+    
+    let tamanhoAtualFonte = 1;
+    
+    aumentaFonteBotao.addEventListener('click', function(){
+    tamanhoAtualFonte += 0.1;
+    document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    });
+    
+    diminuiFonteBotao.addEventListener('click', function(){
+    tamanhoAtualFonte -= 0.1;
+    document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    });
+    });
